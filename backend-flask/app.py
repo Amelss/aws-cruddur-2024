cd7c7e329aafc8f29f51a4142d850e995396fc4b
Ameley K <ameleyka@gmail.com>
2024-04-12 20:55:47 +0000

    commeting out logging to save on spend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -25,12 +25,12 @@ import watchtower
 import logging
 from time import strftime
 
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
 
 
 provider = TracerProvider()
